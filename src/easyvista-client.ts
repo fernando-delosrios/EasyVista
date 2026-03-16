6d8e54fc42950c479526cd64aad674cf58e8efb3
Fernando de los Ríos Sánchez <fernando.delosrios@sailpoint.com>
2026-03-16 15:15:16 +0100

    Update package version to 0.2.1, refactor dependencies, and remove deprecated easyvista.ts file. Adjust README title casing and enhance date handling in index.ts. Introduce new axios-cache-interceptor and axios-request-throttle packages for improved request management.

---
+++ src/easyvista-client.ts
@@ -0,0 +1,201 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from 'axios'
+import axiosThrottle from 'axios-request-throttle'
+import axiosRetry from 'axios-retry'
+import { retriesConfig, throttleConfig } from './axios'
+import { AxiosCacheInstance, setupCache } from 'axios-cache-interceptor'
+import { ACCOUNT_ATTRIBUTES } from './constants'
+
+export class EasyVistaClient {
+    private client: AxiosCacheInstance
+    private search: string
+
+    constructor(config: any) {
+        const baseConfig: AxiosRequestConfig = {
+            baseURL: config.url,
+        }
+        if (config.auth === 'auth.basic') {
+            baseConfig.auth = {
+                username: config['basic.username'],
+                password: config['basic.password'],
+            }
+        } else {
+            baseConfig.headers = {
+                Authorization: `Bearer ${config['token.value']}`,
+            }
+        }
+        const client = axios.create(baseConfig)
+        axiosRetry(client, retriesConfig)
+        axiosThrottle.use(client, throttleConfig)
+        this.client = setupCache(client)
+        this.search = config.search
+    }
+
+    async testConnection(): Promise<AxiosResponse> {
+        const url = `/license`
+
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async listEmployees(max_rows?: number): Promise<AxiosResponse> {
+        const url = `/employees`
+        const fields = ACCOUNT_ATTRIBUTES.join(',')
+        const search = this.search
+
+        if (!max_rows) {
+            const response = await this.listEmployees(1)
+            max_rows = response.data.total_record_count as number
+        }
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+            params: {
+                max_rows,
+                fields,
+                search,
+            },
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async listGroups(max_rows?: number): Promise<AxiosResponse> {
+        const url = `/groups`
+
+        if (!max_rows) {
+            const response = await this.listEmployees(1)
+            max_rows = response.data.total_record_count as number
+        }
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+            params: {
+                max_rows,
+            },
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async getGroup(id: string): Promise<AxiosResponse> {
+        const url = `/groups/${id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async getAccount(id: string): Promise<AxiosResponse> {
+        const url = `/employees/${id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async getGroupMembership(id: string): Promise<AxiosResponse> {
+        const url = `/employees/${id}/groups`
+
+        const request: AxiosRequestConfig = {
+            method: 'get',
+            url,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async setProfil(id: string, profil: number): Promise<AxiosResponse> {
+        const url = `/employees/${id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'put',
+            url,
+            data: {
+                profil_id: profil,
+            },
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async updateAccount(id: string, data: any): Promise<AxiosResponse> {
+        const url = `/employees/${id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'put',
+            url,
+            data,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async createAccount(account: any): Promise<AxiosResponse> {
+        const url = `/employees`
+
+        const request: AxiosRequestConfig = {
+            method: 'post',
+            url,
+            data: {
+                employees: [account],
+            },
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async addGroupMember(group_id: string, employee_id: string): Promise<AxiosResponse> {
+        const url = `/groups/${group_id}/employees/${employee_id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'post',
+            url,
+            data: {},
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+
+    async removeGroupMember(group_id: string, employee_id: string): Promise<AxiosResponse> {
+        const url = `/groups/${group_id}/employees/${employee_id}`
+
+        const request: AxiosRequestConfig = {
+            method: 'delete',
+            url,
+        }
+
+        const response = await this.client.request(request)
+
+        return response
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -24,34 +24,17 @@ import {
     StdEntitlementReadInput,
     StdEntitlementReadOutput,
     AttributeChangeOp,
+    StdTestConnectionInput,
 } from '@sailpoint/connector-sdk'
-import { EasyVistaClient } from './easyvista'
+import { EasyVistaClient } from './easyvista-client'
 import { Account } from './model/account'
 import { Group } from './model/group'
-import { format, parse, subDays } from 'date-fns'
+import { format, parse, parseISO, subDays } from 'date-fns'
 import { AxiosResponse } from 'axios'
+import { CONTRACT_DATES, PROCESSINGWAIT } from './constants'
 
-const INPUT_DATE_FORMAT = 'dd/MM/yyyy'
-const OUTPUT_DATE_FORMAT = 'yyyy-MM-dd'
-
-// const buildEmployee = (attributes: Attributes): any => {
-//     const employee = {
-//         identification: attributes.IDENTIFICATION,
-//         last_name: attributes.LAST_NAME,
-//         login: attributes.LOGIN,
-//         phone_number: attributes.PHONE_NUMBER,
-//         e_mail: attributes.E_MAIL,
-//         location_id: attributes.LOCATION_ID,
-//         department_id: attributes.DEPARTMENT_ID,
-//     }
-
-//     return employee
-// }
-
-const urlToID = (url: string): string => {
-    const id = url.split('/').pop()
-    return id ? id : ''
-}
+const CREATE_DATE_FORMAT = 'dd/MM/yyyy'
+const READ_DATE_FORMAT = 'yyyy-MM-dd'
 
 // Connector must be exported as module property named connector
 export const connector = async () => {
@@ -61,40 +44,75 @@ export const connector = async () => {
     // Use the vendor SDK, or implement own client as necessary, to initialize a client
     const client = new EasyVistaClient(config)
 
+    const urlToID = (url: string): string => {
+        const id = url.split('/').pop()
+        return id ? id : ''
+    }
+
     const buildAccount = async (rawAccount: any): Promise<Account> => {
         const account = new Account(rawAccount)
         const response = await client.getGroupMembership(account.identity)
-        account.attributes.GROUPS = response.data.groups.map((x: string) => urlToID(x))
+        account.attributes.GROUPS = response.data.groups.map(urlToID)
         if (account.attributes.BEGIN_OF_CONTRACT !== '') {
-            const date = parse(account.attributes.BEGIN_OF_CONTRACT as string, OUTPUT_DATE_FORMAT, Date.now())
-            const dateString = format(date, INPUT_DATE_FORMAT)
+            const date = parse(account.attributes.BEGIN_OF_CONTRACT as string, READ_DATE_FORMAT, Date.now())
             if (Date.now() - date.valueOf() > 0) {
                 account.disabled = true
             }
-            account.attributes.BEGIN_OF_CONTRACT = dateString
+            account.attributes.BEGIN_OF_CONTRACT = date.toISOString()
         }
         if (account.attributes.END_OF_CONTRACT !== '') {
-            const date = parse(account.attributes.END_OF_CONTRACT as string, OUTPUT_DATE_FORMAT, Date.now())
-            const dateString = format(date, INPUT_DATE_FORMAT)
-            account.attributes.END_OF_CONTRACT = dateString
+            const date = parse(account.attributes.END_OF_CONTRACT as string, READ_DATE_FORMAT, Date.now())
+            account.attributes.END_OF_CONTRACT = date.toISOString()
         }
 
         return account
     }
 
+    const processInput = (input: any): any => {
+        const account = { ...input }
+        for (const date of CONTRACT_DATES) {
+            const originalDate = account.attributes[date]
+            let newDate
+            try {
+                newDate = parse(originalDate, CREATE_DATE_FORMAT, new Date())
+            } catch (error) {
+                newDate = parse(originalDate, READ_DATE_FORMAT, new Date())
+            }
+            account.attributes[date] = newDate
+        }
+        delete account.GROUPS
+
+        return account
+    }
+
+    const send = async <T>(res: Response<T>, output: T) => {
+        logger.info(output)
+        res.send(output)
+    }
+
     return createConnector()
-        .stdTestConnection(async (context: Context, input: undefined, res: Response<StdTestConnectionOutput>) => {
+        .stdTestConnection(async (context: Context, input: StdTestConnectionInput, res: Response<StdTestConnectionOutput>) => {
             logger.info('Running test connection')
             const response = await client.testConnection()
             res.send({})
         })
         .stdAccountList(async (context: Context, input: StdAccountListInput, res: Response<StdAccountListOutput>) => {
-            const response = await client.listEmployees()
+            const interval = setInterval(() => {
+                res.keepAlive()
+            }, PROCESSINGWAIT)
 
-            for (const rawAccount of response.data.records) {
-                const account = await buildAccount(rawAccount)
-                logger.info(account)
-                res.send(account)
+            const responses: Promise<any>[] = []
+            try {
+                const response = await client.listEmployees()
+
+                for (const rawAccount of response.data.records) {
+                    const account = buildAccount(rawAccount)
+                    responses.push(account)
+                    account.then((x) => send(res, x))
+                }
+            } finally {
+                await Promise.all(responses)
+                clearInterval(interval)
             }
         })
         .stdAccountRead(async (context: Context, input: StdAccountReadInput, res: Response<StdAccountReadOutput>) => {
@@ -114,15 +132,14 @@ export const connector = async () => {
             async (context: Context, input: StdAccountCreateInput, res: Response<StdAccountCreateOutput>) => {
                 logger.info(input)
                 const groups = [].concat(input.attributes.GROUPS)
-                const employee = input.attributes
-                delete employee.GROUPS
+                const employee = processInput(input.attributes)
 
                 let response = await client.createAccount(employee)
                 const HREF: string = response.data.HREF
                 const EMPLOYEE_ID = urlToID(HREF)
 
-                if (groups && groups.length > 0) {
-                    for (const group of groups) {
+                for (const group of groups) {
+                    if (group) {
                         response = await client.addGroupMember(group, EMPLOYEE_ID)
                     }
                 }
@@ -152,12 +169,12 @@ export const connector = async () => {
                                 }
                                 break
                             case 'PROFIL_ID':
+                                response = await client.getAccount(input.identity)
+                                employee.LAST_NAME = response.data.LAST_NAME
                                 if (change.op === AttributeChangeOp.Remove) {
                                     employee.PROFIL_ID = ''
-                                    employee.EMPLOYEE_ID = input.identity
                                 } else {
                                     employee.PROFIL_ID = change.value
-                                    employee.EMPLOYEE_ID = input.identity
                                 }
                                 response = await client.updateAccount(input.identity, employee)
                                 break
@@ -189,7 +206,7 @@ export const connector = async () => {
                 logger.info(input)
                 const today = new Date()
                 const yesterday = subDays(today, 1)
-                const date = format(yesterday, INPUT_DATE_FORMAT)
+                const date = format(yesterday, CREATE_DATE_FORMAT)
 
                 const employee = {
                     END_OF_CONTRACT: date,
